a222a42b4e4614b260760f18eedca1dcff37d25f
BRIAN TUM <bryok2@gmail.com>
2018-04-15 14:05:49 +0300

    adding nav bar and css

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,5 +1,5 @@
 class AssignmentsController < ApplicationController
-
+	before_action :authenticate_user!,except:[:index, :show]
 	before_action :find_assignment, only:[:show, :edit, :destroy, :update]
 	def index
 		if params[:course].blank?
