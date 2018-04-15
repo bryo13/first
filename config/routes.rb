4de8c21c069d84f2caaad791e88cc871274f1715
BRIAN TUM <bryok2@gmail.com>
2018-04-15 17:02:28 +0300

    Adding about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+ root 'about#show'
+ get 'about/show'
   devise_for :users
  resources :assignments
+ authenticated :user do
  root 'assignments#index'
+end
 
 end
